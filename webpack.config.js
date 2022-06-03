df95fa3f4b1c509977834e9d8018b3893cbe8ff5
Kamil Malesa <kamil.malesa1@gmail.com>
2022-06-03 18:03:48 +0200

    webpack config fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,83 @@
-const { merge } = require("webpack-merge");
+// const { merge } = require("webpack-merge");
 
-const commonConfiguration = require("./webpack/common");
+// const commonConfiguration = require("./webpack/common");
 
-module.export = (_env, { mode }) => {
-  const properlyConfig = require(`./webpack/${mode}`);
-  const mergedConfig = merge(commonConfiguration, properlyConfig);
+// module.export = (_env, { mode }) => {
+//   const properlyConfig = require(`./webpack/${mode}`);
+//   const mergedConfig = merge(commonConfiguration, properlyConfig);
 
-  return mergedConfig;
+//   return mergedConfig;
+// };
+
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
+const SRC_DIR = __dirname + "/src";
+const DIST_DIR = __dirname + "/public";
+
+module.exports = {
+  entry: [SRC_DIR + "/index.js"],
+  output: {
+    path: DIST_DIR,
+    filename: "[name].[contenthash:6].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(scss|sass|css)$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              modules: {
+                localIdentName: "[name]__[local]___[hash:base64:5]",
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(html)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "html-loader",
+          options: { minimize: true },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: DIST_DIR + "/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css",
+    }),
+  ],
+  devServer: {
+    static: "./public",
+    hot: true,
+    port: 3000,
+  },
 };
