53738923756b99aa8e1f978fa962cb7ac73afc5e
Kamil Malesa <kamil.malesa1@gmail.com>
2022-06-07 19:43:40 +0200

    added AsideMenu with subcomponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-
-import "./App.scss";
+import { HashRouter as Router } from "react-router-dom";
+import bemCssModules from "bem-css-modules";
+import { default as AppStyles } from "./App.scss";
 
 import StoreProvider from "./store/StoreProvider";
 
+import AsideMenu from "./components/AsideMenu/AsideMenu";
 import Header from "./components/Header";
+
+const style = bemCssModules(AppStyles);
 
 const App = () => {
   return (
     <StoreProvider>
       <Header />
+      <Router>
+        <div className={style()}>
+          <AsideMenu />
+        </div>
+      </Router>
     </StoreProvider>
   );
 };
